05b56485a616404a01d2f84f1f14e38de3598313
CodeBroke33 <minedom33@gmail.com>
2024-02-15 09:20:52 +0200

    again update

--- config/index.js
+++ config/index.js
@@ -1,1 +1,11 @@
-IMPO
+import { createPool } from "mysql";
+import { config } from "dotenv"; 
+config();
+let connection = createPool({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    multipleStatements: true,
+    connectionLimit:30
+})
